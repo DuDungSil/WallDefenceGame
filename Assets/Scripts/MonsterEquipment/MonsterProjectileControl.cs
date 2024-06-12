cbe6bccd9472c80ac09a1b442e108ef89eb03c52
B-JoCharm <jesang0227@gmail.com>
2024-06-12 21:01:03 +0900

    Commit
    commit

--- Assets/Scripts/MonsterEquipment/MonsterProjectileControl.cs
+++ Assets/Scripts/MonsterEquipment/MonsterProjectileControl.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class MonsterProjectileControl : MonsterWeaponDamage
 {
     Rigidbody _rigidbody;
+    public float projectileLifeTime = 0;
 
     // Start is called before the first frame update
     void Start()
     {
+        if(projectileLifeTime != 0) StartCoroutine(DestroyAfterTime());
         _rigidbody = GetComponent<Rigidbody>();
         StartCoroutine(RotateWithVelocity());
     }
@@ -17,6 +19,30 @@ public class MonsterProjectileControl : MonsterWeaponDamage
     void Update()
     {
         
+    }
+    protected virtual void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.layer == LayerMask.NameToLayer("Terrain"))
+        {
+            Destroy(transform.root.gameObject);
+        }
+        if(other.gameObject.layer == LayerMask.NameToLayer("Structure"))
+        {
+            Destroy(transform.root.gameObject);
+        }
+        if(other.gameObject.layer == LayerMask.NameToLayer("Bulidable"))
+        {
+            Destroy(transform.root.gameObject);
+        }          
+    }
+
+    IEnumerator DestroyAfterTime()
+    {
+        // lifetime 만큼 대기
+        yield return new WaitForSeconds(projectileLifeTime);
+
+        // 총알 제거
+        Destroy(transform.root.gameObject);
     }
 
     
